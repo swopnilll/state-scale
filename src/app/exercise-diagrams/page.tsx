310f17460d0ac56180acaed63debf08700fba778
David Khourshid <davidkpiano@gmail.com>
2025-06-03 10:23:30 -0500

    Add page for diagrams

---
+++ src/app/exercise-diagrams/page.tsx
@@ -0,0 +1,9 @@
+export default function Diagrams() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h2 className="text-2xl font-bold">
+        Diagrams (No UI here! Jump into <code>flows.md</code> to get started)
+      </h2>
+    </div>
+  );
+}
